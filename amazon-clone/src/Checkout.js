b3fb651471adfc05a79871d8a4a9d78ecc8c6155
hemantzope23 <hemantzope21@gmail.com>
2021-02-03 14:06:09 +0530

    deploy

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -5,7 +5,7 @@ import { useStateValue } from './StateProvider';
 import Subtotal from './Subtotal';
 
  function Checkout() {
-     const[{ basket }, dispatch] = useStateValue();
+     const[{ basket, user }, dispatch] = useStateValue();
     return (
         <div className="checkout">
             <div className="checkout_left">
@@ -13,8 +13,8 @@ import Subtotal from './Subtotal';
                  src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
                  alt="" />
             
-
                 <div> 
+                    <h3>Hello, {user?.email}</h3>
                     <h1 className="checkout_title">
                         Your shopping Basket
                     </h1>
